a8045fbe1624c775a4a1623e2d2e5dd9313e3853
claynaut <hello@jspescas.io>
2021-11-13 18:09:48 -0800

    Add more features for first page of queries: show user information

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -7,7 +7,8 @@ import {
   BiCheckboxSquare,
   BiDetail,
   BiX,
-  BiDotsVerticalRounded
+  BiExpand,
+  BiCollapse
 } from 'react-icons/bi'
 
 const fetcher = (url: string) => fetch(url).then(res => res.json())
@@ -17,7 +18,7 @@ export default function Landing() {
   const [selectedView, setSelectedView] = useState('All Users')
   const [selectedUsers, setSelectedUsers] = useState([])
   const [allSelected, setAllSelected] = useState(false)
-  const [expandedUser, setExpandedUser] = useState({})
+  const [expandedUsers, setExpandedUsers] = useState([])
 
   const viewOptions = [
     'All Users',
@@ -29,8 +30,9 @@ export default function Landing() {
 
   const selectView = (view: string) => {
     setAllSelected(false)
-    setSelectedUsers([{}])
+    setSelectedUsers([])
     setSelectedView(view)
+    setExpandedUsers([])
   }
 
   const toggleSelectAllUsers = (selectAll: boolean) => {
@@ -54,6 +56,29 @@ export default function Landing() {
     }
     else {
       setSelectedUsers([])
+    }
+  }
+
+  const toggleExpandAllUsers = (expandAll: boolean) => {
+    if (expandAll) {
+      if (selectedView === 'All Users') {
+        setExpandedUsers(data.users)
+      }
+      else if (selectedView === 'Not Applied') {
+        setExpandedUsers(data.users.filter(user => !user.uid))
+      }
+      else if (selectedView === 'Pending') {
+        setExpandedUsers(data.users.filter(user => user.qualified === ''))
+      }
+      else if (selectedView === 'Approved') {
+        setExpandedUsers(data.users.filter(user => user.qualified === 'yeah'))
+      }
+      else if (selectedView === 'Rejected') {
+        setExpandedUsers(data.users.filter(user => user.qualified === 'nope'))
+      }
+    }
+    else {
+      setExpandedUsers([])
     }
   }
 
@@ -99,7 +124,7 @@ export default function Landing() {
             </li>
           </ul>
           <h3>View Options</h3>
-          <div className='grid grid-cols-5 gap-4'>
+          <div className='grid grid-cols-5 gap-2'>
             { viewOptions.map((option) =>
               <motion.button
                 whileHover={{ scale: 1.03}} 
@@ -115,58 +140,149 @@ export default function Landing() {
             )}
           </div>
           <div className='flex mt-3 text-2xl'>
-            <div className='p-2 cursor-pointer' onClick={() => toggleSelectAllUsers(!allSelected)}>
+            <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer' onClick={() => toggleSelectAllUsers(!allSelected)}>
               {
-                allSelected ? <BiCheckboxSquare title='Select' /> : <BiCheckbox title='Select' />
+                allSelected ? <BiCheckboxSquare title='Select All' /> : <BiCheckbox title='Select All' />
               }
             </div>
-            <div className='p-2 cursor-pointer'>
-              <BiDotsVerticalRounded title='More Options' onClick={() => console.log(selectedUsers)} />
+            <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer' onClick={() => toggleExpandAllUsers(!(expandedUsers.length > 0))}>
+              {
+                expandedUsers.length > 0 ?
+                <BiCollapse title='Collapse All' />
+                :
+                <BiExpand title='Expand All' />
+              }
             </div>
           </div>
           {/* all users */}
           {
             selectedView === viewOptions[0] &&
             <div className='flex flex-col gap-2 mt-3'>
-              <div className='grid grid-cols-7 p-2 rounded-md bg-gray-300 font-semibold'>
-                <div className='col-start-2 col-span-3'>
-                  UID
-                </div>
-                <div className='col-span-3'>
+              <div className='grid grid-cols-12 rounded-md bg-gray-300 font-semibold'>
+                <div className='col-start-3 col-span-8 py-2'>
                   Email
                 </div>
               </div>
               { data.users.map((user, idx) =>
-                <motion.div
-                  whileHover={{ y: -2 }}
-                  className={
-                    'relative group grid grid-cols-7 p-2 border-2 rounded-md bg-white shadow-md cursor-pointer '
-                    + (selectedUsers.includes(user) ? 'border-accent-primary' : '')
-                  }
+                <motion.div 
+                whileHover={{ y: -2 }}
+                  className='relative group'
                 >
-                  <div 
+                  <div
                     className={
-                      'text-2xl group-hover:text-black '
-                      + (selectedUsers.includes(user) ? 'text-black' : 'text-gray-400')
+                      'border-2 rounded-md bg-white shadow-md cursor-pointer transition-size duration-150 overflow-hidden '
+                      + (selectedUsers.includes(user) ? 'border-accent-primary ' : ' ')
+                      + (expandedUsers.includes(user) ? 'h-60' : 'h-11')
                     }
                   >
-                    {
-                      selectedUsers.includes(user) ?
-                      <BiCheckboxSquare title='Select' onClick={() => setSelectedUsers(selectedUsers.filter(selectedUser => selectedUser !== user))}/>
-                      :
-                      <BiCheckbox title='Select' onClick={() => setSelectedUsers(selectedUsers.concat([user]))}/>
-                    }
-                  </div>
-                  <div className='col-span-3'>
-                    {user.uid ? user.uid : 'null'}
-                  </div>
-                  <div className='col-span-3'>
-                    {user.email}
-                  </div>
-                  <div className='absolute bottom-[-1px] right-[-3.25rem] invisible group-hover:visible pl-2 cursor-default'>
-                    <div className='p-2 border-2 hover:border-accent-primary rounded-md bg-white shadow-md text-gray-400 hover:text-accent-primary cursor-pointer'>
-                      <BiDetail title='View Details' className='text-2xl'/>
+                    <div className='grid grid-cols-12'>
+                      <div>
+                        <div 
+                          className={
+                            'max-w-min p-2 rounded-full hover:bg-gray-100 text-2xl group-hover:text-black '
+                            + (selectedUsers.includes(user) ? 'text-black' : 'text-gray-400')
+                          }
+                          onClick={
+                            () => setSelectedUsers(
+                              selectedUsers.includes(user) ? 
+                                selectedUsers.filter(selectedUser => selectedUser !== user) :
+                                selectedUsers.concat([user])
+                            )
+                          }
+                        >
+                          {
+                            selectedUsers.includes(user) ?
+                            <BiCheckboxSquare title='Select'/>
+                            :
+                            <BiCheckbox title='Select'/>
+                          }
+                        </div>
+                      </div>
+                      <div className='col-start-3 col-span-8 py-2'>
+                        {user.email}
+                      </div>
+                      <div className='flex items-center col-span-2 py-1 text-sm font-semibold'>
+                        {
+                          user.qualified === '' &&
+                          <div className='rounded-full bg-gray-200 text-gray-500 p-1 px-2'>
+                            PENDING
+                          </div>
+                        }
+                        {
+                          user.qualified === 'yeah' &&
+                          <div className='rounded-full bg-green-200 text-green-700 p-1 px-2'>
+                            APPROVED
+                          </div>
+                        }
+                        {
+                          user.qualified === 'nope' &&
+                          <div className='rounded-full bg-red-200 text-red-700 p-1 px-2'>
+                            REJECTED
+                          </div>
+                        }
+                      </div>
                     </div>
+                    <div className='py-4 border-t-2'>
+                      { user.uid ?
+                        <div className='grid grid-cols-12'>
+                          <div className='col-start-3 col-span-10'>
+                            <ul>
+                              <li>
+                                <b>UID:</b> {user.uid}
+                              </li>
+                              <li>
+                                <b>Full Name:</b> {user.name.first} {user.name.last}
+                              </li>
+                              <li>
+                                <b>School:</b> {user.school}
+                              </li>
+                              <li>
+                                <b>Grade:</b> {user.grade}
+                              </li>
+                              <li>
+                                <b>Graduation Date:</b> {user.graduationDate}
+                              </li>
+                              <li>
+                                <b>App Status: </b>
+                                { user.qualified === '' && (user.criteriaMet ?
+                                  'Pending Approval'
+                                  :
+                                  'Pending Rejection'
+                                )}
+                                { user.qualified !== '' && (user.qualified === 'yeah' ?
+                                  'Approved'
+                                  :
+                                  'Rejected'
+                                )}
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                        :
+                        <div className='text-center text-gray-400'>
+                          No information available yet. User has yet to apply.
+                        </div>
+                      }
+                    </div>
+                  </div>
+                  <div className='absolute top-[-1px] right-[-3rem] invisible group-hover:visible pl-2 cursor-default'>
+                    {
+                      expandedUsers.includes(user) ?
+                      <div
+                        className='p-2 border-2 hover:border-accent-primary rounded-md bg-white shadow-md text-2xl text-gray-400 hover:text-accent-primary cursor-pointer'
+                        onClick={() => setExpandedUsers(expandedUsers.filter(expandedUser => expandedUser !== user))}
+                      >
+                        <BiX title='Close Details'/>
+                      </div>
+                      :
+
+                      <div
+                        className='p-2 border-2 hover:border-accent-primary rounded-md bg-white shadow-md text-2xl text-gray-400 hover:text-accent-primary cursor-pointer'
+                        onClick={() => setExpandedUsers(expandedUsers.concat([user]))}
+                      >
+                        <BiDetail title='View Details'/>
+                      </div>
+                    }
                   </div>
                 </motion.div>
               )}

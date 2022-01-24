313c7d623d3c42d3b25554668cfb8919fb439765
claynaut <hello@jspescas.io>
2022-01-24 15:06:44 -0800

    Add caption

--- components/Admin/UserActions.tsx
+++ components/Admin/UserActions.tsx
@@ -91,61 +91,74 @@ export function UserActions({
   }
 
   return (
-    <div className='flex gap-2 items-center mt-3 text-2xl'>
-      <div
-        className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'
-        onClick={() => toggleSelectAllUsers(!allSelected)}
-      >
-        {
-          allSelected ? <BiCheckboxSquare title='Select All' /> : <BiCheckbox title='Select All' />
-        }
-      </div>
-      <div
-        className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'
-        onClick={() => toggleExpandAllUsers(!(expandedUsers.length > 0))}
-      >
-        {
-          expandedUsers.length > 0 ?
-          <BiCollapse title='Collapse All' />
-          :
-          <BiExpand title='Expand All' />
-        }
-      </div>
+    <>
       {
-        selectedUsers.length > 0 && (selectedView == viewOptions[1] || selectedView == viewOptions[2]) &&
-        <div className='flex gap-1 items-center pl-2 border-l-2'>
-          { selectedView === viewOptions[1] &&
-            <div
-              className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'
-              onClick={() => remindToApply(selectedUsers)}
-            >
-              <BiBell title='Remind Selected to Apply' />
-            </div>
-          }
-          { selectedView === viewOptions[2] &&
-            <>
-              <div
-                className='p-2 rounded-full hover:text-blue-500 hover:bg-blue-100 cursor-pointer'
-                onClick={() => autoReviewSelected(selectedUsers)}
-              >
-                <BiHighlight title='Auto-Review Selected' />
-              </div>
-              <div
-                className='p-2 rounded-full hover:text-green-500 hover:bg-green-100 cursor-pointer'
-                onClick={() => approveSelected(selectedUsers, true)}
-              >
-                <BiTask title='Approve Selected' />
-              </div>
-              <div
-                className='p-2 rounded-full hover:text-red-500 hover:bg-red-100 cursor-pointer'
-                onClick={() => rejectSelected(selectedUsers, false)}
-              >
-                <BiTaskX title='Reject Selected' />
-              </div>
-            </>
+        (selectedView === 'Not Applied' || selectedView === 'Pending') &&
+        <p className='mt-3 font-normal text-base'>
+          <span className='font-medium'>Tip:</span> Select a row to perform more actions.
+        </p>
+      }
+      <div
+        className={
+          'flex gap-2 items-center text-2xl '
+          + (!(selectedView === 'Not Applied' || selectedView === 'Pending') ? 'mt-3' : '')
+        }
+      >
+        <div
+          className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'
+          onClick={() => toggleSelectAllUsers(!allSelected)}
+        >
+          {
+            allSelected ? <BiCheckboxSquare title='Select All' /> : <BiCheckbox title='Select All' />
           }
         </div>
-      }
-    </div>
+        <div
+          className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'
+          onClick={() => toggleExpandAllUsers(!(expandedUsers.length > 0))}
+        >
+          {
+            expandedUsers.length > 0 ?
+            <BiCollapse title='Collapse All' />
+            :
+            <BiExpand title='Expand All' />
+          }
+        </div>
+        {
+          selectedUsers.length > 0 && (selectedView == viewOptions[1] || selectedView == viewOptions[2]) &&
+          <div className='flex gap-1 items-center pl-2 border-l-2'>
+            { selectedView === viewOptions[1] &&
+              <div
+                className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'
+                onClick={() => remindToApply(selectedUsers)}
+              >
+                <BiBell title='Remind Selected to Apply' />
+              </div>
+            }
+            { selectedView === viewOptions[2] &&
+              <>
+                <div
+                  className='p-2 rounded-full hover:text-blue-500 hover:bg-blue-100 cursor-pointer'
+                  onClick={() => autoReviewSelected(selectedUsers)}
+                >
+                  <BiHighlight title='Auto-Review Selected' />
+                </div>
+                <div
+                  className='p-2 rounded-full hover:text-green-500 hover:bg-green-100 cursor-pointer'
+                  onClick={() => approveSelected(selectedUsers, true)}
+                >
+                  <BiTask title='Approve Selected' />
+                </div>
+                <div
+                  className='p-2 rounded-full hover:text-red-500 hover:bg-red-100 cursor-pointer'
+                  onClick={() => rejectSelected(selectedUsers, false)}
+                >
+                  <BiTaskX title='Reject Selected' />
+                </div>
+              </>
+            }
+          </div>
+        }
+      </div>
+    </>
   )
 }

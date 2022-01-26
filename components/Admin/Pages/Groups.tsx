d6b313ae5b0fead900985b2ecba7aa0754178314
claynaut <hello@jspescas.io>
2022-01-26 15:14:38 -0800

    Finish search for groups

--- components/Admin/Pages/Groups.tsx
+++ components/Admin/Pages/Groups.tsx
@@ -3,44 +3,53 @@ import { BiSearch, BiX } from 'react-icons/bi'
 import { GroupBox, GroupActions } from '@/components/Admin'
 
 export function Groups({ data }) {
-  const [selectedView, setSelectedView] = useState('List')
   const [expandedGroups, setExpandedGroups] = useState([])
   const [searchFilter, setSearchFilter] = useState('')
   const [searchQuery, setSearchQuery] = useState(Object)
   const [validSearch, setValidSearch] = useState(false)
-  const [filter, setFilter] = useState('Default')
-  const [sorted, setSorted] = useState(false)
 
   const toggleExpandAllGroups = (expandAll: boolean) => {
-    if (expandAll) {
-      setExpandedGroups([])
-    }
-    else {
-      setExpandedGroups([])
-    }
-  }
-
-  const toggleView = () => {
-    setSelectedView(selectedView === 'List' ? 'Grid' : 'List')
+    if (expandAll) { setExpandedGroups(data.groups) }
+    else { setExpandedGroups([]) }
   }
 
   // regex for searching users by uid, email, or name
-  const regex = /(uid|name):\s*[a-zA-Z0-9._]+/
+  const regex = /(gid|uid|name|email):\s*[a-zA-Z0-9._]+/
 
   // check if a search matches
-  const groupMatch = (doc) => {
+  const groupMatch = (group) => {
+    var gidMatch = true
     var uidMatch = true
     var nameMatch = true
+    var emailMatch = true
     var match = true
-    var [first_name, last_name, uid] = doc.name.split('###')
-    if (searchQuery.uid) {
-      if (!uid.includes(searchQuery.uid)) { uidMatch = false }
+    
+    if (searchQuery.gid) {
+      if (!group.gid.includes(searchQuery.gid)) { gidMatch = false }
     }
-    if (searchQuery.name) {
-      var full_name = (first_name + last_name).toLowerCase().replace(/\s/g, '')
-      if (!(full_name.includes(searchQuery.name.toLowerCase().replace(/\s/g, '')))) { nameMatch = false }
+
+    var anyUIDMatch = false
+    var anyNameMatch = false
+    var anyEmailMatch = false
+    for (let i = 0; i < group.users.length; i++) {
+      const user = group.users[i]
+      if (searchQuery.uid) {
+        if (user.id.includes(searchQuery.uid)) { anyUIDMatch = true }
+      }
+      if (searchQuery.name) {
+        var full_name = (user.name.first + user.name.last).toLowerCase().replace(/\s/g, '')
+        if (full_name.includes(searchQuery.name.toLowerCase().replace(/\s/g, ''))) { anyNameMatch = true }
+      }
+      if (searchQuery.email) {
+        if (user.email && user.email.toLowerCase().includes(searchQuery.email.toLowerCase())) { anyEmailMatch = true }
+      }
     }
-    if (!uidMatch || !nameMatch) { match = false }
+
+    if (searchQuery.uid) { uidMatch = anyUIDMatch }
+    if (searchQuery.name) { nameMatch = anyNameMatch }
+    if (searchQuery.email) { emailMatch = anyEmailMatch }
+
+    if (!gidMatch || !uidMatch || !nameMatch || !emailMatch) { match = false }
     return match
   }
 
@@ -76,11 +85,35 @@ export function Groups({ data }) {
   
   return (
     <>
+      <div className='flex'>
+        <div className='w-full mb-3'>
+          <div className='w-full flex items-center pl-2 border-2 border-gray-400 rounded-md'>
+            <BiSearch className='text-2xl text-gray-500'/>
+            <input
+              className='w-full ml-2 py-2 outline-0 rounded-tr-md rounded-br-md'
+              value={searchFilter}
+              onChange={handleSearchFilter}
+            />
+            { searchFilter.length > 0 &&
+              <div
+                className='p-2 rounded-full text-2xl text-gray-500 hover:bg-gray-100 cursor-pointer'
+                onClick={() => { setSearchFilter(''); setSearchQuery('') }}
+              >
+                <BiX title='Clear Search' />
+              </div>
+            }
+          </div>
+          { searchFilter.length > 0 && !validSearch &&
+            <p className='mb-0 text-sm'>Not a valid search.</p>
+          }
+          { searchFilter.length === 0 &&
+            <p className='mb-0 text-sm'>Search for a group by a user's GID, UID, name, or email. Note that GID and UID is case-sensitive while name is not.</p>
+          }
+        </div>
+      </div>
       <GroupActions
         expandedGroups={expandedGroups}
         toggleExpandAllGroups={toggleExpandAllGroups}
-        toggleView={toggleView}
-        selectedView={selectedView}
       />
       <div className='flex flex-col gap-2 mt-3'>
         { !validSearch ? data.groups.map((group) => 

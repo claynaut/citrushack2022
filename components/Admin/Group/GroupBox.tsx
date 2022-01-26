96ff5940342652d1fd14e03bb2890e82a79f2ef5
claynaut <hello@jspescas.io>
2022-01-26 14:58:24 -0800

    Finish displaying groups

--- components/Admin/Group/GroupBox.tsx
+++ components/Admin/Group/GroupBox.tsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion'
 import { MdArrowRight, MdArrowDropDown } from 'react-icons/md'
+import { BiTrash } from 'react-icons/bi'
 
 export function GroupBox({ group, expandedGroups, setExpandedGroups }) {
   return (
@@ -10,7 +11,11 @@ export function GroupBox({ group, expandedGroups, setExpandedGroups }) {
       <div
         className={
           'border-2 rounded-md bg-white shadow-md cursor-pointer transition-size duration-150 overflow-hidden '
-          + (expandedGroups.includes(group) ? 'h-56 ' : 'h-11 ')
+          + (!expandedGroups.includes(group) ? 'h-11 ' : ' ')
+          + ((expandedGroups.includes(group) && group.users.length === 1) ? 'h-[7.625rem] ' : ' ')
+          + ((expandedGroups.includes(group) && group.users.length === 2) ? 'h-40 ' : ' ')
+          + ((expandedGroups.includes(group) && group.users.length === 3) ? 'h-[12.375rem] ' : ' ')
+          + ((expandedGroups.includes(group) && group.users.length === 4) ? 'h-[14.75rem] ' : ' ')
         }
       >
         <div className='flex'>
@@ -49,10 +54,49 @@ export function GroupBox({ group, expandedGroups, setExpandedGroups }) {
             </div>
           </div>
         </div>
-        <div className='py-4 border-t-2'>
-          <div className='mt-14 text-center text-gray-400'>
-            No information available yet. User has yet to apply.
+        <div className='border-t-2 text-sm'>
+          <div className='flex w-full'>
+            <div className='w-10 p-2 border-b-2 text-center'>#</div>
+            <div className='w-full grid grid-cols-12 py-1.5 border-b-2'>
+              <div className='col-span-4'>Name</div>
+              <div className='col-span-4'>Email</div>
+              <div className='col-span-4'>UID</div>
+            </div>
+            <div className='w-10 p-2 border-b-2 text-center'/>
           </div>
+          {
+            group.users.map((user, idx) =>
+              <div className='flex w-full'>
+                <div
+                  className={
+                    'w-10 p-2 text-center '
+                    + (idx+1 < group.users.length ? 'border-b-2' : '')
+                  }
+                >
+                  {idx+1}
+                </div>
+                <div
+                  className={
+                    'w-full grid grid-cols-12 py-2 '
+                    + (idx+1 < group.users.length ? 'border-b-2' : '')
+                  }
+                >
+                  <div className='col-span-4'>{user.name.first + ' ' + user.name.last}</div>
+                  <div className='col-span-4'>{user.email}</div>
+                  <div className='col-span-4'>{user.id}</div>
+                </div>
+                <div
+                  className={
+                    'text-center text-xl '
+                    + (idx+1 < group.users.length ? 'border-b-2' : '')
+                  }
+                >
+                  <div className='w-9 p-2 rounded-full hover:bg-red-100 hover:text-red-600'>
+                    <BiTrash />
+                  </div>
+                </div>
+              </div>
+          )}
         </div>
       </div>
     </motion.div>

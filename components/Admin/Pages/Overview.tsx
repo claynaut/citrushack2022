6e975f4084ce9ca0e6afcffae07f4760561df01b
claynaut <hello@jspescas.io>
2022-01-26 03:11:49 -0800

    Ignore whitespace when searching for names

--- components/Admin/Pages/Overview.tsx
+++ components/Admin/Pages/Overview.tsx
@@ -264,8 +264,8 @@ export function Overview({ data }) {
     }
     if (searchQuery.name) {
       if (user.name && user.name.first && user.name.last) {
-        var full_name = user.name.first + ' ' + user.name.last
-        if (!(full_name.toLowerCase().includes(searchQuery.name.toLowerCase()))) { nameMatch = false }
+        var full_name = (user.name.first + user.name.last).toLowerCase().replace(' ', '')
+        if (!(full_name.includes(searchQuery.name.toLowerCase().replace(' ', '')))) { nameMatch = false }
       }
       else { nameMatch = false }
     }

f0cd36c43dcc4fcaa7defea7b441910b73e8bb72
claynaut <hello@jspescas.io>
2022-03-01 23:16:00 -0800

    Rename section

--- pages/sections/support.tsx
+++ pages/sections/support.tsx
@@ -24,7 +24,7 @@ export default function Support() {
 
   return (
     <section className='flex flex-col w-full h-screen min-h-[60rem] max-w-[60rem] justify-center items-center'>
-      <h1>Support</h1>
+      <h1>Get Involved</h1>
       <p className='mb-10 text-center'>
         Not interested in hacking but still want to participate? Great! 
         We would love to have you on board. Fill out the forms below if

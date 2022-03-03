0f652ab52d8e4c4ebdee867daf2ee3343f943b15
claynaut <hello@jspescas.io>
2022-03-03 12:48:02 -0800

    Orchestrate tracks animation

--- components/TrackBlocks/components.tsx
+++ components/TrackBlocks/components.tsx
@@ -1,74 +1,93 @@
 import React, { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
-import { motion } from 'framer-motion'
+import { motion, useAnimation } from 'framer-motion'
 import Image from 'next/image'
 
 interface Props {
   image: string,
   title: string,
-  animation: number[]
+  idx: number,
+  animate: boolean
 }
 
-export const Block = ({ image, title, animation }: Props) => (
-  <div className='relative flex flex-col items-center'>
-    <motion.div
-      animate={{ y: animation }}
-      transition={{
-        repeat: Infinity,
-        duration: 10
-      }}
-      className='transform-gpu'
-    >
-      <Image
-        src={image}
-        width={320}
-        height={320}
-        objectFit='contain'
-      />
-    </motion.div>
-    <h4 className='absolute bottom-8 md:bottom-4 font-normal'>
-      {title}
-    </h4>
-  </div>
-)
+export function Block ({ image, title, idx, animate }: Props) {
+  const controls = useAnimation()
+
+  useEffect(() => {
+    if (animate) {
+      controls.start(i => ({
+        y: [0, -20, 0],
+        transition: {
+          duration: 4.375,
+        },
+      }))
+    }
+  }, [animate])
+
+  return (
+    <div className='relative flex flex-col items-center'>
+      <motion.div
+        custom={idx}
+        animate={controls}
+        className='transform-gpu'
+      >
+        <Image
+          src={image}
+          width={320}
+          height={320}
+          objectFit='contain'
+        />
+      </motion.div>
+      <h4 className='absolute bottom-8 md:bottom-4 font-normal'>
+        {title}
+      </h4>
+    </div>
+  )
+}
 
 const tracks = [
   {
     image: '/assets/tracks/diversity-light.svg',
     imageDark: '/assets/tracks/diversity-dark.svg',
     title: 'Diversity & Inclusion',
-    animation: [0, -20, 0, 0],
   },
   {
     image: '/assets/tracks/sustainability-light.svg',
     imageDark: '/assets/tracks/sustainability-dark.svg',
     title: 'Sustainability',
-    animation: [0, 0, -20, 0, 0],
   },
   {
     image: '/assets/tracks/health-light.svg',
     imageDark: '/assets/tracks/health-dark.svg',
     title: 'Health & Wellness',
-    animation: [0, 0, 0, -20, 0],
   },
 ]
 
 export function TrackBlocks() {
+  const [blockIdx, setBlockIdx] = useState(0)
   const [mounted, setMounted] = useState(false)
   const { theme } = useTheme()
 
-  useEffect(() => setMounted(true), [])
+  useEffect(() => {
+    setMounted(true)
+    const timer = setTimeout(() => {
+      setBlockIdx(blockIdx == tracks.length - 1 ? 0 : blockIdx + 1)
+    }, 4375)
+  
+    return () => clearTimeout(timer)
+  }, [blockIdx, setBlockIdx])
 
   if (!mounted) return null
 
   return (
     <div className='flex flex-wrap justify-center gap-y-6'>
-      { tracks.map(({ image, imageDark, title, animation }) => 
+      { tracks.map(({ image, imageDark, title }, idx) => 
           <Block
             key={image}
             image={theme === 'light' ? image : imageDark}
             title={title}
-            animation={animation}
+            idx={idx}
+            animate={Boolean(blockIdx === idx)}
           />
         )
       }

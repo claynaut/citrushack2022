ac36f09c68121698d29e0de1152b7b99ffdc6be7
claynaut <hello@jspescas.io>
2022-03-02 20:45:04 -0800

    Update description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <meta name='description' content='Citrus Hack, a 36-hour hackathon hosted at University of California, Riverside.' />
+          <meta name='description' content='Citrus Hack, a 24-hour hackathon hosted at University of California, Riverside.' />
           <link href='https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' rel='stylesheet' />
           <link rel='icon' href='/favicon.ico' />
           <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />

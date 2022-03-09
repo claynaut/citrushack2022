7f3f6bf63f3f8ea57ca804e663ce80d8af8577ba
claynaut <hello@jspescas.io>
2022-03-08 19:05:05 -0800

    Make count public

--- pages/api/users/count.ts
+++ pages/api/users/count.ts
@@ -1,18 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import clientPromise from '@/lib/mongodb'
-import { getSession } from 'next-auth/react'
 
 export default async function countUsers(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getSession({ req })
   const db = (await clientPromise).db(process.env.MONGODB_DB)
 
-  if (session) {
-    const users = await db.collection('users').find().toArray()
-    const numUsers = Object.keys(users).length
+  const users = await db.collection('users').find().toArray()
+  const numUsers = Object.keys(users).length
 
-    res.status(200).json({ numUsers })
-  }
-  else {
-    res.status(401).json({})
-  }
+  res.status(200).json({ numUsers })
 }

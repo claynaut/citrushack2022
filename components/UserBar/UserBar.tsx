552f69383d529c4b842c07f1a0a6a39e804fa477
rajbirjohar <rajbirjohar1@gmail.com>
2022-03-12 10:21:49 -0800

    Removed email sign in for now

--- components/UserBar/UserBar.tsx
+++ components/UserBar/UserBar.tsx
@@ -49,7 +49,7 @@ export function UserBar() {
       </div>
       <Modal
         title='Sign In'
-        description='Sign in to Citrus Hack via email or Google to apply and access more. No password required.'
+        description='Sign in to Citrus Hack via Google to apply and access more. No password required.'
         show={signinModalOpen}
         handler={setSigninModalOpen}
       >

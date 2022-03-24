38233a96243d9a186ee0144358a39df196a9d696
claynaut <hello@jspescas.io>
2022-03-24 08:51:34 -0700

    Fix food stats, add another stat

--- components/Admin/Pages/Statistics.tsx
+++ components/Admin/Pages/Statistics.tsx
@@ -1,25 +1,28 @@
 import { ShirtStats, FoodStats, ParticipationStats } from '../Statistics'
 
 export function Statistics({ data }) {
-  var numTotal = Object.keys(data.users.filter(user => user.qualified === 'yeah')).length
-  var numXXS = Object.keys(data.users.filter(user => user.qualified === 'yeah' && user.shirtSize === 'XXS')).length
-  var numXS = Object.keys(data.users.filter(user => user.qualified === 'yeah' && user.shirtSize === 'XS')).length
-  var numS = Object.keys(data.users.filter(user => user.qualified === 'yeah' && user.shirtSize === 'S')).length
-  var numM = Object.keys(data.users.filter(user => user.qualified === 'yeah' && user.shirtSize === 'M')).length
-  var numL = Object.keys(data.users.filter(user => user.qualified === 'yeah' && user.shirtSize === 'L')).length
-  var numXL = Object.keys(data.users.filter(user => user.qualified === 'yeah' && user.shirtSize === 'XL')).length
-  var numXXL = Object.keys(data.users.filter(user => user.qualified === 'yeah' && user.shirtSize === 'XXL')).length
-  var numMeat = Object.keys(data.users.filter(user => user.qualified === 'yeah' && user.foodPreference === 'Meat')).length
-  var numVegetarian = Object.keys(data.users.filter(user => user.qualified === 'yeah' && user.foodPreference === 'Vegetarian')).length
-  var numVegan = Object.keys(data.users.filter(user => user.qualified === 'yeah' && user.foodPreference === 'Vegan')).length
-  var numOnline = Object.keys(data.users.filter(user => user.qualified === 'yeah' && user.participation === 'Online')).length
-  var numInPerson = Object.keys(data.users.filter(user => user.qualified === 'yeah' && user.participation === 'In-Person')).length
+  const qualifiedUsers = data.users.filter(user => user.qualified === 'yeah')
+  var numTotal = Object.keys(qualifiedUsers).length
+  var numXXS = Object.keys(qualifiedUsers.filter(user => user.shirtSize === 'XXS')).length
+  var numXS = Object.keys(qualifiedUsers.filter(user => user.shirtSize === 'XS')).length
+  var numS = Object.keys(qualifiedUsers.filter(user => user.shirtSize === 'S')).length
+  var numM = Object.keys(qualifiedUsers.filter(user => user.shirtSize === 'M')).length
+  var numL = Object.keys(qualifiedUsers.filter(user => user.shirtSize === 'L')).length
+  var numXL = Object.keys(qualifiedUsers.filter(user => user.shirtSize === 'XL')).length
+  var numXXL = Object.keys(qualifiedUsers.filter(user => user.shirtSize === 'XXL')).length
+  var numMeat = Object.keys(qualifiedUsers.filter(user => user.foodPreference === 'Meat' && user.participation === 'In-Person')).length
+  var numVegetarian = Object.keys(qualifiedUsers.filter(user => user.foodPreference === 'Vegetarian' && user.participation === 'In-Person')).length
+  var numVegan = Object.keys(qualifiedUsers.filter(user => user.foodPreference === 'Vegan' && user.participation === 'In-Person')).length
+  var numOnline = Object.keys(qualifiedUsers.filter(user => user.participation === 'Online')).length
+  var numInPerson = Object.keys(qualifiedUsers.filter(user => user.participation === 'In-Person')).length
+  var numUCR = Object.keys(qualifiedUsers.filter(user => (user.school).toUpperCase() === 'UCR' || ((user.school).toUpperCase().includes('UNIVERSITY OF CALIFORNIA') && (user.school).toUpperCase().includes('RIVERSIDE')))).length
 
   return (
     <div className='flex flex-col gap-4'>
       <ParticipationStats
         numOnline={numOnline}
         numInPerson={numInPerson}
+        numUCR={numUCR}
         numTotal={numTotal}
       />
       <div className='grid grid-cols-2 gap-4'>
--- components/Admin/Statistics/ParticipationStats.tsx
+++ components/Admin/Statistics/ParticipationStats.tsx
@@ -3,24 +3,30 @@ import { StatsBlob } from './StatsBlob'
 interface Props {
   numOnline: number
   numInPerson: number
+  numUCR: number
   numTotal: number
 }
 
-export function ParticipationStats({ numOnline, numInPerson, numTotal }: Props) {
+export function ParticipationStats({ numOnline, numInPerson, numUCR, numTotal }: Props) {
   return (
-    <div className='grid grid-cols-2 gap-4 rounded-2xl'>
-      <div className='col-span-2 bg-sub-secondary rounded-2xl text-center'>
+    <div className='grid grid-cols-3 gap-4 rounded-2xl'>
+      <div className='col-span-3 bg-sub-secondary rounded-2xl text-center'>
         <h4 className='font-medium'>Participation Statistics</h4>
       </div>
       <StatsBlob
         num={numOnline}
         numTotal={numTotal}
-        label='Online Participants'
+        label='Online'
       />
       <StatsBlob
         num={numInPerson}
         numTotal={numTotal}
-        label='In-Person Participants'
+        label='In-Person'
+      />
+      <StatsBlob
+        num={numUCR}
+        numTotal={numTotal}
+        label='From UCR'
       />
     </div>
   )

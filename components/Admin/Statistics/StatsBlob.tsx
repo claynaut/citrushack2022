948db518cc496629ac21cac8226b6e2010ed2abc
claynaut <hello@jspescas.io>
2022-03-30 16:11:15 -0700

    Update color

--- components/Admin/Statistics/StatsBlob.tsx
+++ components/Admin/Statistics/StatsBlob.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 export const StatsBlob = ({ num, numTotal, label }: Props) => (
-  <div className='flex flex-col items-center grid grid-cols-2 p-4 bg-sub rounded-2xl text-center'>
+  <div className='flex flex-col items-center grid grid-cols-2 p-4 bg-sub rounded-2xl text-center text-sub-bright'>
     <div>
       <h3 className='font-medium text-xl md:text-3xl'>{num}</h3>
       <p className='m-0 text-sm md:text-base'>{label}</p>

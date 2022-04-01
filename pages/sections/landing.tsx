b807da8a8b818759cfa7fa1ba38dc2fca61a0b2c
claynaut <hello@jspescas.io>
2022-04-01 00:10:36 -0700

    Size adjustment

--- pages/sections/landing.tsx
+++ pages/sections/landing.tsx
@@ -87,7 +87,7 @@ export default function Landing() {
               </motion.button>
             </span>
           }
-          <div className='flex flex-col gap-3'>
+          <div className='flex flex-col w-full gap-3'>
             {/* uncomment the day before */}
             { status === 'authenticated' 
               && session.user.uid

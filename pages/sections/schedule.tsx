b09cb6630b5092b96f287f42cbb822c3e575ace6
claynaut <hello@jspescas.io>
2022-04-01 23:23:04 -0700

    Update note

--- pages/sections/schedule.tsx
+++ pages/sections/schedule.tsx
@@ -3,8 +3,10 @@ import { MasterSchedule } from '@/components/Schedule'
 export default function Schedule() {
   return (
     <section className='flex flex-col w-full h-full my-12 lg:my-0 max-w-[72rem] justify-center items-center'>
-      <h1 className='text-center'>Tentative Schedule</h1>
-      <p className='text-center italic'><span className='font-semibold'>Note:</span> All times are in PST (Pacific Standard Time)</p>
+      <h1 className='text-center'>Schedule</h1>
+      <p className='px-4 py-3 bg-card shadow-md rounded-md text-center italic font-medium text-highlight'>
+        Schedule is in PST (Pacific Standard Time)
+      </p>
       <MasterSchedule />
     </section>
   )
